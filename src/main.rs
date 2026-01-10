f3eeaa3ef8000716df38226f5e58167ff407e6f7
frchandra <nismara.chandra@gmail.com>
2026-01-10 21:06:59 +0900

    feat: loop variation

--- src/main.rs
+++ src/main.rs
@@ -109,3 +109,108 @@ fn clone(){
     let name2 = name1.clone(); //keyword "clone". this causes heavy computation
     println!("{} {}", name1, name2);
 }
+
+#[test]
+fn loop_expression(){
+    let mut count = 0;
+    loop{
+        count += 1;
+        if count > 10{
+            break;
+        } else if count % 2 == 0 {
+            continue;
+        }
+
+        println!("counter {}", count);
+    }
+}
+#[test]
+fn loop_with_return_val(){
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter > 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("{}", result);
+}
+
+#[test]
+fn loop_with_label(){
+    let mut num = 1;
+    'outer: loop {
+        let mut i = 1;
+        loop{
+            if num > 10 {
+                break 'outer;
+            }
+
+            println!("{} x {} = {}", num, i, num * i);
+            i += 1;
+            if i > 10 {
+                break;
+            }
+        }
+        num += 1;
+    }
+}
+
+#[test]
+fn while_loop() {
+    let mut counter = 0;
+    while counter <= 10 {
+        if counter % 2 == 0 {
+            println!("Counter : {}", counter);
+        }
+
+        counter += 1;
+    }
+}
+
+#[test]
+fn array_iteration() {
+    let array: [&str; 5] = ["A", "B", "C", "D", "E"];
+    let mut index = 0;
+
+    while index < array.len() {
+        println!("Value : {}", array[index]);
+        index += 1;
+    }
+}
+
+#[test]
+fn array_iteration_for_loop() {
+    let array: [&str; 5] = ["A", "B", "C", "D", "E"];
+
+    for value in array {
+        println!("Value : {}", value);
+    }
+}
+
+#[test]
+fn range() {
+    let range = 0..5;
+    println!("Start : {}", range.start);
+    println!("End : {}", range.end);
+
+    let array: [&str; 5] = ["A", "B", "C", "D", "E"];
+
+    for i in range {
+        println!("{}", array[i]);
+    }
+}
+
+#[test]
+fn range_inclusive() {
+    let range = 0..=4;
+    println!("Start : {}", range.start());
+    println!("End : {}", range.end());
+
+    let array: [&str; 5] = ["A", "B", "C", "D", "E"];
+
+    for i in range {
+        println!("{}", array[i]);
+    }
+}
